10ef491ce5a201126ef6c5eb19925732a6c604df
intenseG <intenseg1515@gmail.com>
2021-10-22 12:11:57 +0900

    ページ内リンクをjs側で処理するよう修正、カルーセル関連の処理をクラス化

--- index.js
+++ index.js
@@ -16,58 +16,85 @@ function changeHeaderColor() {
 
 window.addEventListener("scroll", changeHeaderColor);
 
+let linkArticleListElm = document.getElementById('link_article_list');
+let linkPopularArticleElm = document.getElementById('link_popular_article');
+let linkSnsShareElm = document.getElementById('link_sns_share');
+
 function clickLink(elm) {
     const targetElm = document.getElementById(elm);
     const rect = targetElm.getBoundingClientRect();
-    // const elmTop = rect.top + window.pageYOffset;
     const elmTop = rect.top + window.pageYOffset - headerElm.scrollHeight;
     document.documentElement.scrollTop = elmTop;
+    console.log(elmTop);
 }
 
-const carouselListElms = document.getElementsByClassName('carousel_list');
-let carouselAreaElm = document.getElementById('carousel_area');
+linkArticleListElm.addEventListener("click", function() {
+    clickLink('article_list');
+});
+linkPopularArticleElm.addEventListener("click", function() {
+    clickLink('popular_article');
+});
+linkSnsShareElm.addEventListener("click", function() {
+    clickLink('sns_share');
+});
 
-const width = carouselListElms[0].clientWidth;
-const length = carouselListElms.length;
-const margin = 30;
-const slideArea = width * length + margin * (length - 1);
-carouselAreaElm.style.width = slideArea;
+class Carousel {
+    constructor() {
+        this.carouselListElms = document.getElementsByClassName('carousel_list');
+        this.carouselAreaElm = document.getElementById('carousel_area');
+        this.width = this.carouselListElms[0].clientWidth;
+        this.length = this.carouselListElms.length;
+        this.margin = 30;
+        this.slideCurrentNum = 0;
+        this.totalSlideNum = this.carouselListElms.length - 1;
+    }
 
-let slideCurrentNum = 0;
-const totalSlideNum = carouselListElms.length - 1;
+    calcWidth() {
+        const slideArea = this.width * this.length + this.margin * (this.length - 1);
+        this.carouselAreaElm.style.width = slideArea;
+    }
 
-function changeSlide() {
-    carouselAreaElm.style.left = slideCurrentNum * -width - margin * slideCurrentNum + "px";
+    changeSlide() {
+        this.carouselAreaElm.style.left = this.slideCurrentNum * -this.width - this.margin * this.slideCurrentNum + "px";
+    }
+
+    previousCarouselItem() {
+        console.log("slideCurrentNum: " + this.slideCurrentNum);
+        if (this.slideCurrentNum === 0) {
+            // slideCurrentNum = totalSlideNum;
+            carouselBtnLeft.disabled = true;
+        } else {
+            this.slideCurrentNum--;
+            carouselBtnLeft.disabled = false;
+            carouselBtnRight.disabled = false;
+        }
+
+        this.changeSlide();
+    }
+
+    nextCarouselItem() {
+        console.log("slideCurrentNum: " + this.slideCurrentNum);
+        if (this.slideCurrentNum === this.totalSlideNum) {
+            // slideCurrentNum = 0;
+            carouselBtnRight.disabled = true;
+        } else {
+            this.slideCurrentNum++;
+            carouselBtnRight.disabled = false;
+            carouselBtnLeft.disabled = false;
+        }
+
+        this.changeSlide();
+    }
 }
+
+const carousel = new Carousel();
+carousel.calcWidth();
 
 const carouselBtnLeft = document.getElementById('arrow_left');
 const carouselBtnRight = document.getElementById('arrow_right');
-
-function previousCarouselItem() {
-    console.log("slideCurrentNum: " + slideCurrentNum);
-    if (slideCurrentNum === 0) {
-        // slideCurrentNum = totalSlideNum;
-        carouselBtnLeft.disable = false;
-    } else {
-        slideCurrentNum--;
-        carouselBtnLeft.disable = true;
-    }
-
-    changeSlide();
-}
-
-function nextCarouselItem() {
-    console.log("slideCurrentNum: " + slideCurrentNum);
-    if (slideCurrentNum === totalSlideNum) {
-        // slideCurrentNum = 0;
-        carouselBtnRight.disable = false;
-    } else {
-        slideCurrentNum++;
-        carouselBtnRight.disable = true;
-    }
-
-    changeSlide();
-}
-
-carouselBtnLeft.addEventListener("click", previousCarouselItem)
-carouselBtnRight.addEventListener("click", nextCarouselItem)
+carouselBtnLeft.addEventListener("click", function() {
+    carousel.previousCarouselItem();
+});
+carouselBtnRight.addEventListener("click", function() {
+    carousel.nextCarouselItem();
+});
