becee1fa6fec50f68f316f35397e81f613eaceb2
intenseG <intenseg1515@gmail.com>
2021-11-04 12:55:35 +0900

    汎用的なjs処理を外部jsファイル化するよう修正

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import { scrollTop } from './scrollUtil';
+
 let headerElm = document.querySelector(".header");
 let menuBtnElm = document.querySelector(".menu_btn");
 let menuContentElm = document.querySelector(".menu_content");
 
-let scrollFlag = true;
-
 function changeHeaderColor() {
+    let scrollFlag = true;
     if (!scrollFlag) return;
     if (headerElm.scrollHeight < window.scrollY) {
         headerElm.classList.add("change_color");
@@ -22,44 +23,8 @@ function changeHeaderColor() {
     }, 200);
 }
 
-const scrollTop = () => {
-    let backToTopBtnElm = document.querySelector(".back_to_topbtn");
-    let topBtnFlag = true;
-    const topBtnThreshold = 500;
-
-    const init = () => {
-        handleListener();
-        toggleTopBtn();
-    }
-
-    const handleListener = () => {
-        backToTopBtnElm.addEventListener("click", () => {
-            window.scrollTo({
-                top: 0,
-                behavior: "smooth" 
-            });
-        });
-    };
-
-    const toggleTopBtn = () => {
-        if (!topBtnFlag) return;
-        if (topBtnThreshold < window.scrollY) {
-            backToTopBtnElm.classList.add("display_on");
-        } else {
-            topBtnFlag = false;
-            backToTopBtnElm.classList.remove("display_on");
-        }
-
-        setTimeout(() => {
-            topBtnFlag = true;
-        }, 200);
-    };
-
-    return { init: init };
-};
-
 window.addEventListener("scroll", () => {
-    scrollTop().init();
+    scrollTop(".back_to_topbtn").init();
     changeHeaderColor();
 });
 
